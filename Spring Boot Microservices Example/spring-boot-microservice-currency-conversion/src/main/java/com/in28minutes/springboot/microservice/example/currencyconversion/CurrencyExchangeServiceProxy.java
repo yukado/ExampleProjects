b89775c26a21af80e37567eb4c6c0a31ecd5b81b
Yusuf Kagan Dogruyol <yusufkagandogruyol@gmail.com>
2018-05-12 20:20:17 +0200

    Add files via upload

---
+++ Spring Boot Microservices Example/spring-boot-microservice-currency-conversion/src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.in28minutes.springboot.microservice.example.currencyconversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+/**
+ *
+ * @author Root
+ */
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
+public interface CurrencyExchangeServiceProxy {
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  public CurrencyConversionBean retrieveExchangeValue
+    (@PathVariable("from") String from, @PathVariable("to") String to);
+}
